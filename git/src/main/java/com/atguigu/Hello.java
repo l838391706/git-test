5f5d1526338d8610ce4fae28748311b6035fe261
l838391706 <838691706@qq.com>
2020-06-20 10:07:15 +0800

    第一次修改

--- git/src/main/java/com/atguigu/Hello.java
+++ git/src/main/java/com/atguigu/Hello.java
@@ -4,4 +4,9 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("hello");
     }
+
+    @Override
+    public boolean equals(Object obj) {
+        return super.equals(obj);
+    }
 }
